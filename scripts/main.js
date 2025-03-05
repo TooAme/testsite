962a7546f35eef08eaf9231b93116719eab72406
Mecyan <14328140+mecyan@user.noreply.gitee.com>
2025-03-05 17:56:36 +0800

    2

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,7 @@ const myImage = document.querySelector("img");
 myImage.onclick = () => {
     let mySrc = myImage.getAttribute("src");
     if (mySrc === "images/1.png") {
-        myImage.setAttribute("src", "images/2.png");
+        myImage.setAttribute("src", "images/3.png");
     } else {
         myImage.setAttribute("src", "images/1.png");
     }
